f5817e19c5544ae8a8391d58a40b748ec5d6a2bf
PrateetiSurRay <129949748+PrateetiSurRay@users.noreply.github.com>
2023-07-06 10:34:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const collapseBtn = document.getElementById('collapse-btn');
+const rightcollapseBtn = document.getElementById('right-collapse-btn');
 const mainNav= document.getElementById('main-nav');
+const rightNav= document.getElementById('right-nav');
 
 collapseBtn.addEventListener('click', function() {
   mainNav.classList.toggle('collapsed');
+  document.getElementById("container").style.width="100vw";
+});
+rightcollapseBtn.addEventListener('click', function() {
+  rightNav.classList.toggle('rightcollapsed');
 });
